d6f9539ed24f06d339446cbf1aad2595fc748d9e
Alexandre Amat <alex.amat@i-drc.com>
2023-03-16 20:46:39 +0100

    missing header

--- include/utils/debug.h
+++ include/utils/debug.h
@@ -1,14 +1,14 @@
 #ifndef __DEBUG_H__
 #define __DEBUG_H__
 
+#include <libc/time.h>
 #include <stddef.h>
 #include <stdio.h>
 
-#define LOG(fmt, ...)                                             \
-  (printf("(%lu) [%s:%u] " fmt "\n", clock(), __func__, __LINE__, \
-          ##__VA_ARGS__))
+#define LOG(fmt, ...) \
+  printf("(%lu) [%s:%u] " fmt "\n", clock(), __func__, __LINE__, ##__VA_ARGS__)
 
-#define DEBUG_VAR(fmt, a) (LOG("" #a " = " fmt "", a))
+#define DEBUG_VAR(fmt, a) LOG("" #a " = " fmt "", a)
 
 void hexdump(const void* data, size_t size);
 
